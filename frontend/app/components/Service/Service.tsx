f163b53917ee480db91cc6fe4bd5e44596dd4e36
Gnezdilova Margarita <margarita.gnezdilova@kidzania.ru>
2025-12-18 20:52:29 +0300

    completed main page

--- frontend/app/components/Service/Service.tsx
+++ frontend/app/components/Service/Service.tsx
@@ -1,147 +1,99 @@
 import styles from "./Service.module.scss";
-import { useEffect, useRef, useState } from "react";
+import { useState, useMemo } from "react";
 import ServiceItem, {
   type IServiceItemProps,
 } from "../ServiceItem/ServiceItem";
+import { useTranslation } from "react-i18next";
 
 type ITab = {
   id: string;
 } & IServiceItemProps;
 
-const tabs: ITab[] = [
-  {
-    id: "tax",
-    label: "§",
-    title: "Tailored Tax Solutions for Maximum Legal Savings",
-    content: [
-      [
-        "Preparation of income tax, corporation tax, trade tax and sales tax returns",
-        "Consulting Agricultural and Forestry",
-      ],
-      ["Taking over negotiations with the tax office", "Appeals"],
-      ["Obtaining binding information"],
-
-      [
-        "Advice on all tax issues",
-        "Support and encouragement in tax investigations and proceedings",
-      ],
-      ["Control of tax assessments", "Support and advice during tax audits"],
-      ["Tax restructuring", "Company succession"],
-    ],
-  },
-  {
-    id: "accounting",
-    label: "+/-",
-    title: "Accounting",
-    content: [
-      [
-        "Special accounting for agriculture and forestry, car dealerships, craftsmen and health professions",
-      ],
-      [
-        "Preparation and timely submission of advance VAT returns and recapitulative reports",
-      ],
-      [],
-      [
-        "Establishment and management of financial accounting including asset accounting",
-      ],
-      ["Monthly business evaluations and business analyses"],
-    ],
-  },
-  {
-    id: "audit",
-    label: "?",
-    title: "Audit",
-    content: [
-      [
-        "Support and accompaniment in tax investigations and voluntary disclosures",
-      ],
-      [],
-      [],
-      ["Taking over negotiations with the tax office"],
-      ["Support and advice during tax audits"],
-    ],
-  },
-  {
-    id: "consulting",
-    label: "!",
-    title: "Management Consulting",
-    content: [
-      [
-        "Advice on acquisitions or sales of companies as well as on all types of corporate restructuring",
-      ],
-      ["Advice on all financial and business issues"],
-      [],
-      [
-        "Preparation of planned balance sheets and planned profit and loss calculations",
-      ],
-      ["Carrying out company valuations"],
-      [
-        "Business analyses of the net assets, financial position and results of operations",
-      ],
-    ],
-  },
-  {
-    id: "payroll",
-    label: "€",
-    title: "Payroll Accounting",
-    content: [
-      ["Preparation of payroll accounting"],
-      ["Labour cost optimisation nettomaxx"],
-      [],
-      [
-        "Legal advice in cooperation with the law firm Friedrich, Westhues-Wedig & Coll.",
-      ],
-      ["International posting of workers"],
-    ],
-  },
-];
-
 export default function Services() {
-  const [selectedTab, setSelectedTab] = useState(tabs[0]);
-  const [isTransitioning, setIsTransitioning] = useState(false);
-  const timeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+  const { t, ready } = useTranslation();
 
+  const tabs: ITab[] = useMemo(
+    () => [
+      {
+        id: "tax",
+        label: "§",
+        title: t("services.tax.title"),
+        content: t("services.tax.content", {
+          returnObjects: true,
+        }) as string[][],
+      },
+      {
+        id: "accounting",
+        label: "+/-",
+        title: t("services.accounting.title"),
+        content: t("services.accounting.content", {
+          returnObjects: true,
+        }) as string[][],
+      },
+      {
+        id: "audit",
+        label: "?",
+        title: t("services.audit.title"),
+        content: t("services.audit.content", {
+          returnObjects: true,
+        }) as string[][],
+      },
+      {
+        id: "consulting",
+        label: "!",
+        title: t("services.consulting.title"),
+        content: t("services.consulting.content", {
+          returnObjects: true,
+        }) as string[][],
+      },
+      {
+        id: "payroll",
+        label: "€",
+        title: t("services.payroll.title"),
+        content: t("services.payroll.content", {
+          returnObjects: true,
+        }) as string[][],
+      },
+    ],
+    [t]
+  );
+
+  const [selectedTab, setSelectedTab] = useState<ITab>(tabs[0]);
+  const [selectedTabs, setSelectedTabs] = useState<ITab[]>([]);
+
+  // Десктоп
   const handleTabChange = (tab: ITab) => {
     if (tab.id === selectedTab.id) return;
-
-    setIsTransitioning(true);
-
-    if (timeoutRef.current) {
-      clearTimeout(timeoutRef.current);
-    }
-
-    timeoutRef.current = setTimeout(() => {
-      setSelectedTab(tab);
-      setIsTransitioning(false);
-    }, 150);
+    setSelectedTab(tab);
   };
 
-  useEffect(() => {
-    return () => {
-      if (timeoutRef.current) {
-        clearTimeout(timeoutRef.current);
-      }
-    };
-  }, []);
+  // Мобилка
+  function handleTabsChange(tab: ITab) {
+    if (selectedTabs.find((t) => t.id === tab.id)) {
+      setSelectedTabs(selectedTabs.filter((s_t) => s_t.id !== tab.id));
+      return;
+    }
+    setSelectedTabs([...selectedTabs, tab]);
+  }
+
+  if (!ready) return null;
 
   return (
-    <div className={styles.services}>
-      <ul className={styles.tabs}>
-        {tabs.map((tab) => (
-          <li key={tab.id} className={styles.tab}>
-            <button
-              type="button"
-              className={`${styles.tabs__button} ${tab.id === selectedTab.id && styles.tabs__button_active}`}
-              onClick={() => handleTabChange(tab)}
-            >
-              {tab.label}
-            </button>
-          </li>
-        ))}
-      </ul>
-      <div
-        className={`${styles.contentWrapper} ${isTransitioning ? styles.contentWrapper_hidden : ""}`}
-      >
+    <>
+      <div className={styles.services}>
+        <ul className={styles.tabs}>
+          {tabs.map((tab) => (
+            <li key={tab.id} className={styles.tab}>
+              <button
+                type="button"
+                className={`${styles.tabs__button} ${tab.id === selectedTab.id && styles.tabs__button_active}`}
+                onClick={() => handleTabChange(tab)}
+              >
+                {tab.label}
+              </button>
+            </li>
+          ))}
+        </ul>
         <ServiceItem
           key={selectedTab.id}
           label={selectedTab.label}
@@ -149,6 +101,44 @@ export default function Services() {
           content={selectedTab.content}
         />
       </div>
-    </div>
+
+      <div className={styles.services__mobile}>
+        <ul className={styles.tabs__mobile}>
+          {tabs.map((tab) => (
+            <li key={tab.id} className={styles.tab}>
+              <button
+                type="button"
+                className={`${styles.tabs__button__mobile} ${selectedTabs.find((t) => t.id === tab.id) && styles.tabs__button__mobile_active}`}
+                onClick={() => handleTabsChange(tab)}
+              >
+                {tab.title}
+
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="8"
+                  height="13"
+                  viewBox="0 0 8 13"
+                  fill="none"
+                >
+                  <path
+                    d="M0.707031 0.707031L6.36389 6.36389L0.707032 12.0207"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                  />
+                </svg>
+              </button>
+              {selectedTabs.find((t) => t.id === tab.id) && (
+                <ServiceItem
+                  key={tab.id}
+                  label={tab.label}
+                  title={tab.title}
+                  content={tab.content}
+                />
+              )}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 }
--- frontend/app/routes/HomePage/HomePage.tsx
+++ frontend/app/routes/HomePage/HomePage.tsx
@@ -7,11 +7,13 @@ import img4 from "../../images/home/intro/intro-4.png";
 import learnMore from "../../images/icons/learn-more.svg";
 import { useState } from "react";
 import Services from "~/components/Service/Service";
+import { useTranslation } from "react-i18next";
 export function meta({}: Route.MetaArgs) {
   return [{ title: "Home" }, { name: "description", content: "THP" }];
 }
 
 export default function HomePage() {
+  const { t } = useTranslation();
   const [openAboutUs, setOpenAboutUs] = useState(false);
   return (
     <div className={styles.home}>
@@ -19,11 +21,12 @@ export default function HomePage() {
         <div className={styles.section__number}>01</div>
         <div className={styles.section__content}>
           <div className={styles.intro}>
-            <h1 className={styles.intro__title}>
-              THP Steuerkontor: Shape Your Expertise. Secure Your Future.
-            </h1>
+            <h1
+              className={styles.intro__title}
+              dangerouslySetInnerHTML={{ __html: t("homePage.introTitle") }}
+            />
             <div className={styles.intro__imgs}>
-              <div></div>
+              <div className={styles.intro__empty}></div>
               <img src={img1} alt="thp steuerkontor intro 1" />
               <img src={img2} alt="thp steuerkontor intro 2" />
               <img src={img3} alt="thp steuerkontor intro 3" />
@@ -32,46 +35,34 @@ export default function HomePage() {
           </div>
 
           <div className={`wrapper ${styles.aboutUs}`}>
-            <h2 className={styles.h2}>About Us</h2>
+            <div className={styles.aboutUs__inner}>
+              <h2 className={styles.h2}>{t("homePage.aboutUs.title")}</h2>
 
-            <div className={styles.aboutUs__content}>
-              <p className={styles.aboutUs__text}>
-                THP Steuerkontor Steuerberatungsgesellschaft mbH is a locally
-                rooted and nationally active tax consulting company, which was
-                founded in 1994 under the name ADVISA in Schermbeck. Our head
-                office in Schermbeck has been located since over 20 years in the
-                industrial park in Schermbeck.
-              </p>
+              <div className={styles.aboutUs__content}>
+                <p className={styles.aboutUs__text}>
+                  {t("homePage.aboutUs.paragraph1")}
+                </p>
 
-              <p className={styles.aboutUs__text}>
-                In our firm, our clients benefit from more than 20 years of
-                experience, which helps us to meet the diverse complex
-                requirements. THP Steuerkontor Steuerberatungsgesellschaft mbH
-                builds on long-term partnerships. It is active as a service
-                provider for commercial and freelance companies of all
-                industries and legal forms as well as private individuals,
-                associations and foundations.
-              </p>
+                <p className={styles.aboutUs__text}>
+                  {t("homePage.aboutUs.paragraph2")}
+                </p>
+              </div>
+              {!openAboutUs && (
+                <button
+                  type="button"
+                  className={styles.learnMore}
+                  onClick={() => setOpenAboutUs(!openAboutUs)}
+                >
+                  {t("homePage.aboutUs.learnMore")}
+                  <img src={learnMore} alt={t("homePage.aboutUs.learnMore")} />
+                </button>
+              )}
+              {openAboutUs && (
+                <p className={styles.aboutUs__text}>
+                  {t("homePage.aboutUs.paragraph3")}
+                </p>
+              )}
             </div>
-            {!openAboutUs && (
-              <button
-                type="button"
-                className={styles.learnMore}
-                onClick={() => setOpenAboutUs(!openAboutUs)}
-              >
-                Learn More
-                <img src={learnMore} alt="Learn More" />
-              </button>
-            )}
-            {openAboutUs && (
-              <p className={styles.aboutUs__text}>
-                With everything we have to offer in terms of knowledge, skills
-                and commitment, we offer you comprehensive structuring advice in
-                all tax law and business management issues at reasonable fees.
-                Individual solutions and the goal of doing something special for
-                each client characterize our business philosophy.
-              </p>
-            )}
           </div>
         </div>
       </section>
@@ -79,7 +70,9 @@ export default function HomePage() {
       <section className={styles.section}>
         <div className={styles.section__number}>02</div>
         <div className={styles.section__content}>
-          <h2 className={styles.service__title}>Our Services</h2>
+          <h2 className={styles.service__title}>
+            {t("homePage.servicesTitle")}
+          </h2>
         </div>
       </section>
       <Services />
