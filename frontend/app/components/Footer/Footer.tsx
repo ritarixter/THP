ee152456a39ef51126d7d0fe1801de0f88e19d5d
Ksenia <bezdenejnyhk@yandex.ru>
2025-12-21 23:13:13 +0300

    Футер с переводом и адаптивом, хедер с переводом

--- frontend/app/components/Footer/Footer.tsx
+++ frontend/app/components/Footer/Footer.tsx
@@ -1,5 +1,40 @@
+import { Link } from "react-router";
 import styles from "./Footer.module.scss";
+import { useTranslation } from "react-i18next";
+import logo from "../../images/logo.svg";
 
 export default function Footer() {
-  return <footer className={styles.footer}></footer>;
+  const { t } = useTranslation();
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.blockNav}>
+        <div className={styles.column}>
+          <p className={styles.column_title}>{t("footer.company.title")}</p>
+          <p className={styles.column_text}>{t("footer.company.aboutUs")}</p>
+          <p className={styles.column_text}>{t("footer.company.service")}</p>
+          <p className={styles.column_text}>{t("footer.company.vacancies")}</p>
+        </div>
+        <div className={styles.column}>
+          <p className={styles.column_title}>{t("footer.contacts.title")}</p>
+          <p className={styles.column_text}>{t("footer.contacts.contacts")}</p>
+          <p className={styles.column_text}>{t("footer.contacts.team")}</p>
+          <p className={styles.column_text}>{t("footer.contacts.contactUs")}</p>
+        </div>
+        <div className={styles.column}>
+          <p className={styles.column_title}>{t("footer.news.title")}</p>
+          <p className={styles.column_text}>{t("footer.news.latest")}</p>
+        </div>
+      </div>
+      <div className={styles.blockInfo}>
+        <Link to="/" className={styles.logo}>
+          <img src={logo} alt="Logo" />
+        </Link>
+        <p className={styles.location}>{t("footer.title.location")}</p>
+        <div className={styles.info}>
+          <p className={styles.location}>{t("footer.title.phone")}</p>
+          <p className={styles.location}>{t("footer.title.email")}</p>
+        </div>
+      </div>
+    </footer>
+  );
 }
--- frontend/app/components/Header/Header.tsx
+++ frontend/app/components/Header/Header.tsx
@@ -1,39 +1,78 @@
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
 import { useTranslation } from "react-i18next";
 import logo from "../../images/logo.svg";
 import styles from "./Header.module.scss";
+import { Gamburger } from "./Gamburger";
+import { useState } from "react";
+import addres from "../../images/icons/location.svg";
+import email from "../../images/icons/email.svg";
+import lang from "../../images/icons/lang.svg";
 
 export default function Header() {
   const { t } = useTranslation();
+  const [openMenu, setOpenMenu] = useState(false);
+  const location = useLocation();
+
+  const isActive = (path: string) => {
+    return location.pathname === path;
+  };
 
   return (
     <header className={styles.header}>
       <div className={styles.container}>
-        <div className={styles.logo}>
-          <Link to="/">
-            <img src={logo} alt="Logo" />
-          </Link>
+        <div className={styles.button}>
+          <Gamburger open={openMenu} setOpen={setOpenMenu} />
         </div>
+        <Link to="/">
+          <img src={logo} alt="Logo" />
+        </Link>
+        <div className={styles.block}>
+          <p className={styles.location}>
+            <img src={addres} alt="Location" />
+            {t("header.title.location")}
+          </p>
+        </div>
+        <div className={styles.block}>
+          <p className={styles.location}>
+            <img src={lang} alt="Location" />
+            {t("header.title.lang")}
+          </p>
+          <p className={styles.location}>{t("header.title.phone")}</p>
+        </div>
+        <div className={styles.mail}>
+          <img src={email} alt="Email" />
+        </div>
+      </div>
+      {openMenu && (
         <nav className={styles.nav}>
           <ul className={styles.navList}>
-            <li>
-              <Link to="/" className={styles.navLink}>
+            <li className={`${styles.navList_item}`}>
+              <Link
+                to="/"
+                className={`${styles.navLink} ${isActive("/") && styles.active}`}
+              >
                 {t("header.home")}
               </Link>
             </li>
-            <li>
-              <Link to="/contacts" className={styles.navLink}>
+            <li className={styles.navList_item}>
+              <Link
+                to="/contacts"
+                className={`${styles.navLink} ${isActive("/contacts") && styles.active}`}
+              >
                 {t("header.contacts")}
               </Link>
             </li>
-            <li>
-              <Link to="/news" className={styles.navLink}>
+            <li className={styles.navList_item}>
+              <Link
+                to="/news"
+                className={`${styles.navLink} ${isActive("/news") && styles.active}`}
+              >
                 {t("header.news")}
               </Link>
             </li>
           </ul>
         </nav>
-      </div>
+      )}
     </header>
   );
 }
