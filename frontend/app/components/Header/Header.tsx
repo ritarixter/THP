5021cc25482bba7083b598c74af5f820995f6636
Ksenia <bezdenejnyhk@yandex.ru>
2025-12-23 21:20:22 +0300

    Форматирование

--- frontend/app/components/Header/Header.tsx
+++ frontend/app/components/Header/Header.tsx
@@ -7,8 +7,8 @@ import { useEffect, useState } from "react";
 import addres from "../../images/icons/location.svg";
 import email from "../../images/icons/email.svg";
 import lang from "../../images/icons/lang.svg";
-import arrow from '../../images/icons/arrow1.svg'
-import mail from '../../images/icons/MailBlue.svg'
+import arrow from "../../images/icons/arrow1.svg";
+import mail from "../../images/icons/MailBlue.svg";
 
 export default function Header() {
   const { t, i18n } = useTranslation();
@@ -16,7 +16,7 @@ export default function Header() {
   const [currentLanguage, setCurrentLanguage] = useState(i18n.language);
   const location = useLocation();
   const navigate = useNavigate();
-  const [openSubMenu, setOpenSubMenu] = useState(false)
+  const [openSubMenu, setOpenSubMenu] = useState(false);
 
   const languages = [
     { code: "en", name: "ENG" },
@@ -56,79 +56,73 @@ export default function Header() {
   };
 
   const mobileVersion = () => {
-    return <div className={styles.mobile}>
-      <nav className={styles.mobile_nav}>
-        <ul className={styles.mobile_navList}>
-          <li className={`${styles.mobile_navList_item}`}>
-            <Link
-              to=""
-              className={styles.mobile_navLink}
-            >
-              {t("footer.company.title")}
-              <img className={`${styles.arrow} ${openSubMenu && styles.arrow_active}`} onClick={() => setOpenSubMenu(!openSubMenu)} src={arrow}/>
-            </Link>
-          </li>
-          {openSubMenu && <>
-          <li className={`${styles.mobile_navList_i}`}>
-            <Link
-              to="#aboutUs"
-              className={styles.navLink}
-            >
-              {t("footer.company.aboutUs")}
-            </Link>
-          </li>
-          <li className={`${styles.mobile_navList_i}`}>
-            <Link
-              to="#services"
-              className={styles.navLink}
-            >
-              {t("footer.company.service")}
-            </Link>
-          </li>
-          <li className={`${styles.mobile_navList_i}`}>
-            <Link
-              to="#contacts"
-              className={styles.navLink}
-            >
-              {t("footer.company.vacancies")}
-            </Link>
-          </li>
-          </>
-          }
-          <li className={styles.mobile_navList_item}>
-            <Link
-              to="/contacts"
-              className={styles.mobile_navLink}
-            >
-              {t("header.contacts")}
-            </Link>
-          </li>
-          <li className={styles.mobile_navList_item}>
-            <Link
-              to="/news"
-              className={styles.mobile_navLink}
-            >
-              {t("header.news")}
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <div className={styles.mobile_block}>
-         <p className={styles.location_text}>{t("header.title.number")}</p>
-         <p className={styles.location}>{t("header.title.phone")}</p>
+    return (
+      <div className={styles.mobile}>
+        <nav className={styles.mobile_nav}>
+          <ul className={styles.mobile_navList}>
+            <li className={`${styles.mobile_navList_item}`}>
+              <Link to="" className={styles.mobile_navLink}>
+                {t("footer.company.title")}
+                <img
+                  className={`${styles.arrow} ${openSubMenu && styles.arrow_active}`}
+                  onClick={() => setOpenSubMenu(!openSubMenu)}
+                  src={arrow}
+                />
+              </Link>
+            </li>
+            {openSubMenu && (
+              <>
+                <li className={`${styles.mobile_navList_i}`}>
+                  <Link to="#aboutUs" className={styles.navLink}>
+                    {t("footer.company.aboutUs")}
+                  </Link>
+                </li>
+                <li className={`${styles.mobile_navList_i}`}>
+                  <Link to="#services" className={styles.navLink}>
+                    {t("footer.company.service")}
+                  </Link>
+                </li>
+                <li className={`${styles.mobile_navList_i}`}>
+                  <Link to="#contacts" className={styles.navLink}>
+                    {t("footer.company.vacancies")}
+                  </Link>
+                </li>
+              </>
+            )}
+            <li className={styles.mobile_navList_item}>
+              <Link to="/contacts" className={styles.mobile_navLink}>
+                {t("header.contacts")}
+              </Link>
+            </li>
+            <li className={styles.mobile_navList_item}>
+              <Link to="/news" className={styles.mobile_navLink}>
+                {t("header.news")}
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <div className={styles.mobile_block}>
+          <p className={styles.location_text}>{t("header.title.number")}</p>
+          <p className={styles.location}>{t("header.title.phone")}</p>
+        </div>
+        <div className={styles.mobile_block}>
+          <p className={styles.location_text}>{t("header.title.address")}</p>
+          <p className={styles.location}>{t("header.title.location")}</p>
+        </div>
+        <div className={styles.mobile_hel}>
+          <div
+            className={styles.mobile_button}
+            onClick={() => {
+              navigate("#contacts");
+            }}
+          >
+            {t("header.title.contact")}
+            <img src={mail} />
+          </div>
+        </div>
       </div>
-      <div className={styles.mobile_block}>
-         <p className={styles.location_text}>{t("header.title.address")}</p>
-         <p className={styles.location}>{t("header.title.location")}</p>
-      </div>
-      <div className={styles.mobile_hel}>
-      <div className={styles.mobile_button} onClick={()=> {navigate("#contacts")}}>
-        {t("header.title.contact")}
-        <img src={mail} />
-      </div>
-      </div>
-    </div>
-  }
+    );
+  };
 
   return (
     <header className={styles.header}>
@@ -167,7 +161,7 @@ export default function Header() {
           <p className={styles.location}>{t("header.title.phone")}</p>
         </div>
         <div className={styles.mail}>
-          <img src={email} alt="Email" onClick={()=> navigate("#contacts")}/>
+          <img src={email} alt="Email" onClick={() => navigate("#contacts")} />
         </div>
       </div>
       <nav className={styles.nav}>
